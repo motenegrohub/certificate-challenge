2fcf23a67bf3719664a9a47e9b135a551bfb5bfa
jhonatacaiob <jhonatacaiob@gmail.com>
2024-05-11 17:06:30 -0300

    Adiciona rota que permite baixar o arquivo

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from random import randint
 
 from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from playwright.sync_api import sync_playwright
 
@@ -14,7 +15,7 @@ pdf_location = Path(
 
 
 @app.get('/certificate')
-def get_certificate(request: Request):
+def save_certificate_file(request: Request):
     with sync_playwright() as p:
         browser = p.chromium.launch()
 
@@ -28,16 +29,29 @@ def get_certificate(request: Request):
         )
         page.emulate_media(media='print')
         p = page.pdf(format='A4')
-        hash_pdf = sha256(p).hexdigest()
-        page.pdf(path=(pdf_location / f'{hash_pdf}.pdf'))
+
+        hash_code_pdf = sha256(p).hexdigest()
+        page.pdf(path=(pdf_location / f'{hash_code_pdf}.pdf'))
 
     return {
-        'path': request.url_for('func2', hash=hash_pdf)._url,
+        'path': request.url_for('get_certificate_file', hash_code=hash_code_pdf)._url,
         'expiration_date': datetime.now(),
         'number_certificate': randint(10000, 99999),
     }
 
 
-@app.get('/certificate/file/{hash}')
-def func2(hash: int):
-    ...
+@app.get('/certificate/file/{hash_code}')
+def get_certificate_file(hash_code: str, download: bool = False):
+    print(hash_code)
+    pdf_file_location = pdf_location / f'{hash_code}.pdf'
+
+    print(download)
+
+    if download:
+        return FileResponse(
+            pdf_file_location,
+            media_type='application/octet-stream',
+            filename=pdf_file_location.name,
+        )
+    else:
+        return FileResponse(pdf_file_location)
